91873f706aa1b1da31840b88f4c6ef6b7e0ac9c4
indah <142625024+thatdesignlog@users.noreply.github.com>
2025-07-30 17:47:46 -0400

    added my answer for length questions

---
+++ 001 - beginner lvl/02_length.py
@@ -0,0 +1,36 @@
+# from https://github.com/bregman-arie/python-exercises/blob/main/exercises/hello_world/length.md
+# exercise was tested with https://www.online-python.com/
+
+# question list:
+# How to print the length of the string 'abcd' ?
+# How to print the length of the variable x (x is the list [5, 30 ,2]) ?
+# What would be the length of following dictionary {'x': 3, 'y': 3} ?
+# What would be the length of the tuple ('x', 'y') ?
+
+--------
+
+# 01. how to print length of string 'abcd'?
+
+list = 'abcd'
+print(len(list))
+
+# 02. how to print length of the variable x if x is the list of [5,30,2]?
+
+x = [5, 30, 2]
+print(len(x))
+
+# 03. what would be the length of following dictionary {'x': 3, 'y':3}?
+
+random_dictionary =  {
+  'x': 3,
+  'y': 3,
+}
+
+print(len(random_dictionary))
+
+# 04. What would be the length of the tuple ('x', 'y')
+
+random_tuple = ('x', 'y')
+print(len(random_tuple))
+
+
