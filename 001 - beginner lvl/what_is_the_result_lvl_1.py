a33b24ae7ab18be6f6d29d1cdd96d1967e5b1e75
indah <142625024+thatdesignlog@users.noreply.github.com>
2025-07-30 18:08:14 -0400

    created what_is_the_result_lvl_1.py

---
+++ 001 - beginner lvl/what_is_the_result_lvl_1.py
@@ -0,0 +1,8 @@
+# from https://github.com/bregman-arie/python-exercises/blob/main/exercises/hello_world/what_is_the_result_lvl_1.md
+
+# What is the result of each of the following statements?
+# 1 > 2
+# 1 == 1
+# 4 < 1
+# 'two' > 1
+
