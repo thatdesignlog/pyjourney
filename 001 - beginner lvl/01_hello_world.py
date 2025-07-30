41b73774832585b9a8588227fdde8e2c3f4fec7b
indah <142625024+thatdesignlog@users.noreply.github.com>
2025-07-30 17:13:45 -0400

    added new answer & dev-reflection

--- 001 - beginner lvl/01_hello_world.py
+++ 001 - beginner lvl/01_hello_world.py
@@ -4,7 +4,18 @@
 # "Hello
 #   World"
 
+
+# this is how i solved it
+
 first_part = 'Hello'
 second_part = '  World'
 
 print(first_part + second_part)
+
+
+# this is the solution answer from the repo
+
+print('Hello \n World')
+
+
+# dev-reflection: looking at the actual answer, i've realized both work just fine but the repo solution is def easier for this particular question
