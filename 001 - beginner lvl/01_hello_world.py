920ee85a14e3205f5b37517a3e54c1a7a50e3418
indah <142625024+thatdesignlog@users.noreply.github.com>
2025-07-30 17:10:47 -0400

    finished first task! :)

---
+++ 001 - beginner lvl/01_hello_world.py
@@ -0,0 +1,10 @@
+# Write one line of code that will print "Hello World!"
+# Now modify the code so the output looks like this:
+
+# "Hello
+#   World"
+
+first_part = 'Hello'
+second_part = '  World'
+
+print(first_part + second_part)
