8d3685a39e7a180b61747d5d4c111738f988459b
indah <142625024+thatdesignlog@users.noreply.github.com>
2025-07-30 17:59:43 -0400

    added dev-reflection & resources

--- 001 - beginner lvl/02_length.py
+++ 001 - beginner lvl/02_length.py
@@ -11,8 +11,8 @@
 
 # 01. how to print length of string 'abcd'?
 
-list = 'abcd'
-print(len(list))
+meovv = 'abcd'
+print(len(meovv))
 
 # 02. how to print length of the variable x if x is the list of [5,30,2]?
 
@@ -32,5 +32,18 @@ print(len(random_dictionary))
 
 random_tuple = ('x', 'y')
 print(len(random_tuple))
+
+
+# note: there is no solution from the python repo, so these are all my answers
+# dev-reflection: there are some parts that i didn't know but i went to w3 to find resources and tested them with an online IDE before putting in my answers. 
+# dictionary in python seems to have a similar functionality as dictionary in c#. that was my first time learning about a 'tuple'. 
+# now i know how to differentiate list, set, dictionary, and tuple. a tuple uses () and ordered like a list but a tuple is unchangable & a list is changeable. 
+# both tuple & list allow duplication. a set & a dictionary both use {} for their collection and both don't allow duplication. dictionary has {'something': 'another something'}
+# whereas a set is singular item. Once a set is created, you cannot change its items, but you can remove items and add new items. 
+# Dictionaries are changeable, meaning that we can change, add or remove items after the dictionary has been created.
+
+# resources: 
+# a tuple - https://www.w3schools.com/python/python_tuples.asp
+# a dictionary - https://www.w3schools.com/python/python_dictionaries.asp
 
 
